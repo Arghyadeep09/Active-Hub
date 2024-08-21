38acb74fd9429288cd5ff62dcd78bf5f2d7fcf4d
Scrapper700 <113296663+Scrapper700@users.noreply.github.com>
2024-08-21 17:14:12 +0530

    Update HardRepoPage.jsx

--- frontend/src/Pages/HardRepoPage.jsx
+++ frontend/src/Pages/HardRepoPage.jsx
@@ -3,39 +3,42 @@ import Axios from 'axios';
 import './HardRepoPage.css'
 
 const HardRepoPage = () => {
-    const [hardrepositories, setHardrepositories] = useState([]);
+  const [hardrepositories, setHardrepositories] = useState([]);
 
-    useEffect(() => {
-        const fetchRepositories = async () => {
-          try {
-            const response = await Axios.get('https://active-hub.onrender.com/hardRepoData'); 
-            console.log(response)
-            if (response.statusText!=="OK") {
-              throw new Error('Network response was not ok');
-            }
-            setHardrepositories(response.data);
-          } catch (error) {
-            console.error('Error fetching repositories:', error);
-          }
-        };
-    
-        fetchRepositories();
-      }, []);
+  useEffect(() => {
+    const fetchRepositories = async () => {
+      try {
+        const response = await Axios.get('https://active-hub.onrender.com/hardRepoData');
+        console.log(response)
 
-      return (
-        <div className="app">
-        {hardrepositories.map((repo) => (
-          <div className="card" key={repo._id}>
-            <h2>{repo.name}</h2>
-            <p><strong>Owner:</strong> {repo.owner}</p>
-            <p><strong>URL:</strong> <a href={repo.URL} target="_blank" rel="noopener noreferrer">{repo.URL}</a></p>
-            <p><strong>Description:</strong> {repo.description}</p>
-            <p><strong>Tech Stack:</strong> {repo.techStack.join(', ')}</p>
-            <p><strong>Difficulty:</strong> {repo.difficulty}</p>
-          </div>
-        ))}
-      </div>
-      );
+        if (!response) {
+          throw new Error('Network response was not ok');
+          return;
+        }
+        setHardrepositories(response.data);
+      } catch (error) {
+        console.error('Error fetching repositories:', error);
+      }
+    };
+
+    fetchRepositories();
+  }, []);
+
+  return (
+    <div className="app">
+      {hardrepositories && hardrepositories.map((repo) => (
+        <div className="card" key={repo._id}>
+          <h2>{repo.name}</h2>
+          <p><strong>Owner:</strong> {repo.owner}</p>
+          <p><strong>URL:</strong> <a href={repo.URL} target="_blank" rel="noopener noreferrer">{repo.URL}</a></p>
+          <p><strong>Description:</strong> {repo.description}</p>
+          <p><strong>Tech Stack:</strong> {repo.techStack.join(', ')}</p>
+          <p><strong>Difficulty:</strong> {repo.difficulty}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default HardRepoPage
+
